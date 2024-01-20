60f96c531e520b3c9e46f276028aef3f953a0207
rokapet <88285020+rokapet@users.noreply.github.com>
2024-01-20 20:30:57 +0100

    Update eon_csv.sh

--- eon_csv.sh
+++ eon_csv.sh
@@ -37,3 +37,67 @@ do
       echo "Inverter active_energy_import_1.8.0" $epoch $value >> ./elec/import_180.txt
    fi
 
+
+   #reactive_energy_import_3.8.0
+   value=`echo $line2 | cut -d\; -f 8`
+
+   if [ -n "$value" ]
+   then
+      echo "Inverter reactive_energy_import_3.8.0" $epoch $value >> ./elec/import_380.txt
+   fi
+
+   #energy_export_rate
+   value=`echo $line2 | cut -d\; -f 10`
+
+   if [ -n "$value" ]
+   then
+      echo "Inverter energy_export_rate" $epoch $value >> ./elec/export_rate.txt
+   fi
+
+   #energy_import_rate
+   value=`echo $line2 | cut -d\; -f 12`
+
+   if [ -n "$value" ]
+   then
+      echo "Inverter energy_import_rate" $epoch $value >> ./elec/import_rate.txt
+   fi
+
+   #active_energy_export_2.8.1
+   value=`echo $line2 | cut -d\; -f 14`
+
+   if [ -n "$value" ]
+   then
+      echo "Inverter active_energy_export_2.8.1" $epoch $value >> ./elec/export_281.txt
+   fi
+
+   #active_energy_export_2.8.0
+   value=`echo $line2 | cut -d\; -f 16`
+
+   if [ -n "$value" ]
+   then
+      echo "Inverter active_energy_export_2.8.0" $epoch $value >> ./elec/export_280.txt
+   fi
+
+   #active_energy_export_2.8.2
+   value=`echo $line2 | cut -d\; -f 18`
+
+   if [ -n "$value" ]
+   then
+      echo "Inverter active_energy_export_2.8.2" $epoch $value >> ./elec/export_282.txt
+   fi
+
+done < $1
+echo "Beolvasás és szétválogatás vége!"
+echo ""
+mv $1 ./elec_sent/
+
+echo "Beküldés Zabbix-ba:"
+for f in ./elec/*.txt;
+do
+   echo "File beküldése:" $f
+   zabbix_sender -z $ZABBIX_SERVER -p 10051 -i $f -T
+   rm -f $f
+   echo ""
+done
+
+echo "Minden rekord sikeresen beküldve Zabbix-ba! :-)"
